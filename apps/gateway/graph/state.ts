82821cb6bd957b68bb1f49004699c7766274e606
jspmern <jspmern@gmail.com>
2026-06-17 17:38:00 +0530

    added state for graph

---
+++ apps/gateway/graph/state.ts
@@ -0,0 +1,13 @@
+import {
+  StateGraph,
+  StateSchema,
+  MessagesValue,
+  ReducedValue,
+  GraphNode,
+  ConditionalEdgeRouter,
+  START,
+  END,
+} from "@langchain/langgraph";
+const MessagesState = new StateSchema({
+  messages: MessagesValue,
+});
